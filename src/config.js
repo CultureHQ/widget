91228bc4ff6dcdab0828d87dc67bf47aabbc7087
Kevin Deisz <kevin.deisz@gmail.com>
2018-06-19 12:49:52 -0400

    Switch up config to production

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
-// export const API_ROOT = "https://api.culturehq.com";
-export const API_ROOT = "http://localhost:3000";
+export const API_ROOT = "https://api.culturehq.com";
 
-// export const PLATFORM_ROOT = "https://platform.culturehq.com";
-export const PLATFORM_ROOT = "http://localhost:4000";
+export const PLATFORM_ROOT = "https://platform.culturehq.com";
 
 export const PREVIEW_LIMIT = 5;
 
