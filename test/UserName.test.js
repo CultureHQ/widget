3d1a339c8b4b08ac0cdf25ec7e969eb2e360ec49
Kevin Deisz <kevin.deisz@gmail.com>
2018-08-09 10:09:22 -0400

    Tests for UserName

---
+++ test/UserName.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+
+import UserName from "../src/components/UserName";
+
+test("renders a link if the user is active", () => {
+  const user = { active: true, id: 1, name: "Kevin" };
+
+  const component = mount(<UserName user={user} />);
+
+  expect(component.find("a")).toHaveLength(1);
+  expect(component.text()).toContain(user.name);
+});
+
+test("renders just the name if the user is inactive", () => {
+  const user = { active: false, id: 1, name: "Kevin" };
+
+  const component = shallow(<UserName user={user} />);
+
+  expect(component.find("a")).toHaveLength(0);
+  expect(component.text()).toEqual(user.name);
+});
