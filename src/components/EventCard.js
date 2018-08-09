69a0e5e0a80bd37e048918fb21624eb2ec70bfb1
Kevin Deisz <kevin.deisz@gmail.com>
2018-08-09 09:58:24 -0400

    Split out MoreInfo component

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,21 +1,10 @@
 import React from "react";
-import styled from "styled-components";
 
 import EventImage from "./EventImage";
 import EventMetadata from "./EventMetadata";
 import Rsvps from "./Rsvps";
 import { EventContainer, EventHeader, EventFooter } from "./EventDisplay";
-
-const MoreInfo = styled.a`
-  background-color: #79b17d;
-  border-radius: 5px;
-  color: white;
-  flex-grow: 1;
-  float: right;
-  padding: 10px 27px;
-  text-align: center;
-  text-decoration: none;
-`;
+import MoreInfo from "./MoreInfo";
 
 const EventCard = ({ event }) => (
   <EventContainer>
---
+++ src/components/MoreInfo.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const MoreInfo = styled.a`
+  background-color: #79b17d;
+  border-radius: 5px;
+  color: white;
+  flex-grow: 1;
+  float: right;
+  padding: 10px 27px;
+  text-align: center;
+  text-decoration: none;
+`;
+
+export default MoreInfo;
