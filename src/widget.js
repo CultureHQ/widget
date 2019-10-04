ab3b2b4a754afc861cef384cae7f4cf663430f52
Kevin Deisz <kevin.deisz@gmail.com>
2019-10-04 09:20:57 -0400

    s/components/app

--- src/widget.js
+++ src/widget.js
@@ -1,7 +1,7 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 
-const App = require("./components/App").default;
+const App = require("./app/App").default;
 
 // This is purposefully `module.exports`, because if you use `export default`
 // you end up having to use `CHQ.default` instead of just `CHQ`.
